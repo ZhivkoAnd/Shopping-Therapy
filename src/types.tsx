19f269e6185ba67e0161555cc116242548ee3aff
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2023-02-22 22:59:46 +0100

    asd

--- src/types.tsx
+++ src/types.tsx
@@ -1,7 +1,72 @@
-import React from "react";
+export interface BookingProps {
+  id: number;
+  title: string;
+  price: number;
+  image?: string;
+  qty?: number;
+}
 
-const types = () => {
-  return <div>types</div>;
-};
+export interface Product {
+  [data: string]: {
+    cities?: [];
+    id: number;
+    title: string;
+    price: number;
+    image?: string;
+    qty?: number;
+  };
+}
 
-export default types;
+export interface Vacations {
+  params?: {
+    city: string;
+  };
+  items: {
+    fields: {
+      date?: Date;
+      title?: string;
+      slug?: string;
+      gallery?: {
+        fields: {
+          file: {
+            url: string;
+          };
+        };
+      }[];
+    };
+  }[];
+}
+
+export interface VacationPanelProps {
+  item: {
+    fields: {
+      thumbnail: {
+        fields: {
+          file: {
+            url: string;
+            details: {
+              image: {
+                width: number;
+                height: number;
+              };
+            };
+          };
+        };
+      };
+      date: string;
+      title: string;
+      slug: string;
+    };
+  };
+}
+export interface FilterProps {
+  setFilterDateAscending?: () => void;
+  setFilterDateDescending?: () => void;
+  setFilterTitleAscending?: () => void;
+  setFilterTitleDescending?: () => void;
+  setFilterPriceAscending?: () => void;
+  setFilterPriceDescending?: () => void;
+  setInputQuery?: (e: string) => void;
+  inputQuery?: string;
+  isAdminPage?: boolean;
+}
