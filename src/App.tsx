a33262f386330573e0913e0da16337336d189c1a
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2023-02-22 21:17:12 +0100

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
 import Shop from "./components/Shop";
 import Layout from "./components/ui/Layout";
 
@@ -16,26 +14,10 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className={`App ${colorMode}`}>
       <Layout lightMode={lightMode} darkMode={darkMode}>
         <Shop />
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HM
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+        <p>Hello !</p>
       </Layout>
     </div>
   );
