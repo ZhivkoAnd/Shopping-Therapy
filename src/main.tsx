fea242123c9c8fd318a2f614c8a3b45f66768a45
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2023-02-22 21:30:31 +0100

    asd

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./global.scss";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
