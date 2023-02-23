1ed6801c4c91cc6be4ffabf1c2706161e3b7c7a0
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2023-02-23 18:02:43 +0100

    asd

---
+++ src/components/ui/AuthenticationMenu.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React from "react";
+import { UserAuth } from "../../utils/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+
+const AuthenticationMenu = () => {
+  const { user, logout }: any = UserAuth();
+
+  let navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+    } catch (e: any) {
+      console.log(e.message);
+    }
+  };
+
+  return (
+    <>
+      {!user && <Link to="/login">Login</Link>}
+      {user && (
+        <>
+          <h5>Hi {user.email}</h5>
+          <Link to="/login" onClick={handleLogout}>
+            Log Out
+          </Link>
+        </>
+      )}
+    </>
+  );
+};
+
+export default AuthenticationMenu;
--- src/components/ui/Navigation.tsx
+++ src/components/ui/Navigation.tsx
@@ -1,7 +1,139 @@
-import React from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
+import AdbIcon from "@mui/icons-material/Adb";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import AuthenticationMenu from "./AuthenticationMenu";
 
-const Navigation = () => {
-  return <div>Navigation</div>;
+const pages = [
+  {
+    id: 1,
+    label: "Vacations",
+    link: "/",
+  },
+  {
+    id: 2,
+    label: "Shop",
+    link: "/shop",
+  },
+  {
+    id: 3,
+    label: "Admin",
+    link: "/admin",
+  },
+];
+
+interface Props {
+  lightMode: React.MouseEventHandler<SVGSVGElement>;
+  darkMode: React.MouseEventHandler<SVGSVGElement>;
+}
+
+const Navigation = ({ lightMode, darkMode }: Props) => {
+  // const pathname = usePathname();
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [activePage, setActivePage] = useState<any>(1);
+
+  const handleOpenNavMenu = (e: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(e.currentTarget);
+  };
+
+  const handleCloseNavMenu = (id: any) => {
+    pages.map((page) => {
+      if (id === page.id) {
+        return;
+      } else {
+        setActivePage(id);
+      }
+    });
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" className="navbar">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.label} onClick={handleCloseNavMenu}>
+                  <Link to={page.link}>
+                    <Typography textAlign="center">{page.label}</Typography>
+                  </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Link to={page.link} key={page.label}>
+                <Button
+                  onClick={() => handleCloseNavMenu(page.id)}
+                  className={
+                    activePage === page.id
+                      ? "navbar__link-active"
+                      : "navbar__link"
+                  }
+                  sx={{
+                    my: 2,
+
+                    display: "block",
+                  }}
+                >
+                  {page.label}
+                </Button>
+              </Link>
+            ))}
+            <div className="navbar__colorThemeIcons">
+              <LightModeIcon onClick={lightMode} className="sun" />
+              <DarkModeIcon onClick={darkMode} className="moon" />
+            </div>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <AuthenticationMenu />
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
-
 export default Navigation;
