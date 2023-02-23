b3e42d0b3b0e14847352f7cf2fbbdaa929c1add6
Zhivko Andreev <zhia@yousee.dk>
2023-02-23 11:24:39 +0100

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import Shop from "./components/Shop";
 import Admin from "./components/Admin";
 import Layout from "./components/ui/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "../styles/Global.scss";
 import "../styles/Navigation.scss";
 import "../styles/VacationPanel.scss";
 import "../styles/ActionBar.scss";
 import "../styles/ErrorUI.scss";
 import "../styles/Footer.scss";
+
+
 function App() {
   const [colorMode, setColorMode] = useState("dark");
 
@@ -22,9 +25,12 @@ function App() {
   return (
     <div className={`App ${colorMode}`}>
       <Layout lightMode={lightMode} darkMode={darkMode}>
-        <Shop />
-        <Admin />
-        <p>Hello !</p>
+        <BrowserRouter>
+        <Routes>
+        <Route path="/" element={ <Admin/> } />
+        <Route path="shop" element={ <Shop/> } />
+      </Routes>
+      </BrowserRouter>
       </Layout>
     </div>
   );
