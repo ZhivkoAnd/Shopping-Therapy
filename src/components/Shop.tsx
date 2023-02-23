aed0bbfb9fa25920478baeca0c7e4a6e5af8d702
Zhivko Andreev <zhia@yousee.dk>
2023-02-23 09:06:23 +0100

    asd

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,10 +1,23 @@
-import React, { useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  filterPriceAscending,
+  filterPriceDescending,
+  filterTitleAscending,
+  filterTitleDescending,
+} from "../utils/FilterFunctions";
+import LoadingSpinners from "../components/ui/LoadingSpinners";
+import ErrorUI from "../components/ui/ErrorUI";
 
 const Shop = () => {
+
+  const [inputQuery, setInputQuery] = useState("");
+
   const titleRef: any = useRef("");
   const priceRef: any = useRef("");
   const imageRef: any = useRef("");
+
+  const queryClient = useQueryClient();
 
   const fetchBookingQuery = async () => {
     // We receive the response from the server
@@ -17,6 +30,99 @@ const Shop = () => {
       console.log(data);
     },
   });
+
+  const createData = async (data: {}) => {
+    const response = await fetch("http://localhost:4000/cities", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    return response.json();
+  };
+
+  const deleteData = async (id: number) => {
+    const response = await fetch(`http://localhost:4000/cities/${id}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) {
+      throw new Error();
+    }
+    return true;
+  };
+
+  const { mutateAsync: deleteMutate, isLoading: isLoadingDeletedElement } =
+    useMutation(deleteData, {
+      onSettled: (data) => {
+        queryClient.invalidateQueries(["bookings"]);
+      },
+    });
+
+  const remove = async (id: number) => {
+    await deleteMutate(id);
+  };
+
+  const [filterss, setFilters] = useState(data);
+
+  const { mutateAsync: createMutate } = useMutation(createData, {
+    onSettled: (data) => {
+      queryClient.invalidateQueries(["bookings"]);
+    },
+  });
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    createMutate({
+      title: titleRef.current.value,
+      price: priceRef.current.value,
+      image: imageRef.current.value,
+    });
+  };
+
+  const inputData = data?.filter((city: any) =>
+    city.title.toLowerCase().includes(inputQuery.toLowerCase())
+  );
+
+  const setFilterPriceAscending = () => {
+    if (inputData && inputData.length) {
+      setFilters(filterPriceAscending(inputData));
+    }
+  };
+
+  const setFilterPriceDescending = () => {
+    if (inputData && inputData.length) {
+      setFilters(filterPriceDescending(inputData));
+    }
+  };
+
+  const setFilterTitleAscending = () => {
+    if (inputData && inputData.length) {
+      setFilters(filterTitleAscending(inputData));
+    }
+  };
+
+  const setFilterTitleDescending = () => {
+    if (inputData && inputData.length) {
+      setFilters(filterTitleDescending(inputData));
+    }
+  };
+
+  useEffect(() => {
+    if (inputData && inputData.length) {
+      setFilters(inputData);
+    } else {
+      setFilters([]);
+    }
+  }, [inputQuery]);
+
+  if (isLoading) {
+    return <LoadingSpinners magnifying_glass />;
+  }
+
+  if (isError) {
+    return <ErrorUI />;
+  }
 
   return (
     <form className="container">
