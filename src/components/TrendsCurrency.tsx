32a79a2f64fe898a512dcf0ef7f206ead6a12535
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2023-03-01 18:15:06 +0100

    asd

--- src/components/TrendsCurrency.tsx
+++ src/components/TrendsCurrency.tsx
@@ -4,12 +4,9 @@ import { useQueryClient, useQuery } from "@tanstack/react-query";
 const Trends = () => {
   const queryClient = useQueryClient();
 
-  //       `https://api.currencyapi.com/v3/historical?apikey=cQzoqbcrYtVvquShOKnJXnN4ND2H3EoN86os9IQg&currencies=EUR&date=2023-02-27`
-  //      `https://api.currencyapi.com/v3/historical?apikey=cQzoqbcrYtVvquShOKnJXnN4ND2H3EoN86os9IQg&currencies=${currency}&date=${date}`
-
-  const fetchFruitsQuery = async (currency: any, date: any) => {
+  const fetchFruitsQuery = async (date: any, currency: any) => {
     const response = await fetch(
-      `https://api.currencyapi.com/v3/historical?apikey=cQzoqbcrYtVvquShOKnJXnN4ND2H3EoN86os9IQg&currencies=EUR,BGN&date=2023-02-27`
+      `https://api.exchangerate.host/${date}?symbols=${currency}`
     );
     return response.json();
   };
@@ -18,8 +15,11 @@ const Trends = () => {
   const [date, setDate] = useState<any>("2023-02-27");
 
   const { data, isLoading, isError } = useQuery(
-    ["pokemons", currency, date],
-    () => fetchFruitsQuery(currency, date)
+    ["pokemons", date, currency],
+    () => fetchFruitsQuery(date, date),
+    {
+      refetchOnWindowFocus: false,
+    }
   );
 
   console.log(data);
