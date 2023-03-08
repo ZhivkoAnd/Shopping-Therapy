48e35d386718eaeb624ace182a3b31efffbcc579
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2023-03-08 22:56:11 +0100

    asd

--- src/components/RandomCity.tsx
+++ src/components/RandomCity.tsx
@@ -4,6 +4,7 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 const RandomCity = () => {
   const [correctCity, setCorrectCity] = useState<any>("");
   const [answers, setAnswers] = useState<any>("");
+  const [correctAnswer, setCorrectAnswer] = useState("");
 
   const randomCityQuery = async () => {
     const response = await fetch(`${import.meta.env.VITE_API_KEY}/cities`);
@@ -17,12 +18,20 @@ const RandomCity = () => {
     },
   });
 
-  //   const random = () => {
-  //     return data && data[Math.floor(Math.random() * data.length)].title;
-  //   };
+  const randomCity = () => {
+    if (data) {
+      return data[Math.floor(Math.random() * data.length)].title;
+    }
+  };
 
   console.log(correctCity);
-  return <div></div>;
+  console.log(randomCity());
+
+  useEffect(() => {
+    setAnswers([correctCity, randomCity(), randomCity(), randomCity()]);
+  }, [correctCity]);
+
+  return <div>{answers}</div>;
 };
 
 export default RandomCity;
