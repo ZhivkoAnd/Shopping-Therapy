5952a5605c812c162f3c8c10f7abc4e7fe29f8a8
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2023-03-12 15:27:18 +0100

    asd

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,16 +1,8 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 const Slider = () => {
   const [index, setIndex] = useState(1);
-
-  // Fetch specific city
-  const fetchCity = async (index: any) => {
-    const response = await fetch(
-      `${import.meta.env.VITE_API_KEY}/cities/${index}`
-    );
-    return response.json();
-  };
 
   // Fetch all cities
   const fetchCities = async () => {
@@ -18,42 +10,110 @@ const Slider = () => {
     return response.json();
   };
 
-  // To get a specific element from the data received, we have to pass the index from the state to the query function
-  const { data: city } = useQuery(["city", index], () => fetchCity(index));
   const { data: cities } = useQuery(["cities"], fetchCities);
 
-  const nextItem = () => {
-    setIndex((index) => checkNumber(index + 1));
+  const city = cities ? cities[index] : "";
+
+  const nextCity = () => {
+    setIndex((index) => checkCityIndex(index + 1));
   };
 
-  const previousItem = () => {
-    setIndex((index) => checkNumber(index - 1));
+  const previousCity = () => {
+    setIndex((index) => checkCityIndex(index - 1));
   };
 
-  console.log(cities);
-
-  const checkNumber = (number: any) => {
+  const checkCityIndex = (number: any) => {
     // if the number is bigger than the last item in the array, return 0(the first item).
     // It's data.length - 1, because arrays are 0-based. So if it's 0,1,2,3, last item will be the length(4) - 1 = 3
-    if (number > cities.length) {
-      return 1;
+    if (number > cities.length - 1) {
+      return 0;
     }
     // if the number is less than 0, return the last item in the array
     if (number < 1) {
-      return cities.length;
+      return cities.length - 1;
     }
-    // in neither of those cases, return the number
+    // In neither of those cases, return the number
     return number;
+  };
+
+  const randomCity = () => {
+    // Get a random value from 0 and the length
+    let randomNumber = Math.floor(Math.random() * cities.length);
+    // check If that random number and our array length are the same
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    // Finallly we use the checkCity in case the number got bigger than the last item of the array
+    setIndex(checkCityIndex(randomNumber));
   };
 
   return (
     <>
       {city?.title}
-      <button onClick={nextItem}>Next</button>
-      <button onClick={previousItem}>Previous</button>
-      <button>random</button>
+      <button onClick={nextCity}>Next</button>
+      <button onClick={previousCity}>Previous</button>
+      <button onClick={randomCity}>random</button>
     </>
   );
 };
 
 export default Slider;
+
+// import React, { useState } from "react";
+// import { useQuery } from "@tanstack/react-query";
+
+// const Slider = () => {
+//   const [index, setIndex] = useState(1);
+
+//   // Fetch specific city
+//   const fetchCity = async (index: any) => {
+//     const response = await fetch(
+//       `${import.meta.env.VITE_API_KEY}/cities/${index}`
+//     );
+//     return response.json();
+//   };
+
+//   // Fetch all cities
+//   const fetchCities = async () => {
+//     const response = await fetch(`${import.meta.env.VITE_API_KEY}/cities`);
+//     return response.json();
+//   };
+
+//   // To get a specific element from the data received, we have to pass the index from the state to the query function
+//   const { data: city } = useQuery(["city", index], () => fetchCity(index));
+//   const { data: cities } = useQuery(["cities"], fetchCities);
+
+//   const nextItem = () => {
+//     setIndex((index) => checkNumber(index + 1));
+//   };
+
+//   const previousItem = () => {
+//     setIndex((index) => checkNumber(index - 1));
+//   };
+
+//   console.log(cities);
+
+//   const checkNumber = (number: any) => {
+//     // Unlike the previous scenario, where we use the indexes of the array , here we set the id of the objects
+//     if (number > cities.length) {
+//       return 1;
+//     }
+//     // if the number is less than 1, return the object with the last id
+//     if (number < 1) {
+//       return cities.length;
+//     }
+//     // in neither of those cases, return the number
+//     return number;
+//   };
+
+//   return (
+//     <>
+//       {city?.title}
+//       <button onClick={nextItem}>Next</button>
+//       <button onClick={previousItem}>Previous</button>
+//       <button>random</button>
+//     </>
+//   );
+// };
+
+// export default Slider;
